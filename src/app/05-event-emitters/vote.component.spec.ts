e3c5f68a5346b2b194382c02625dc5b0f46aa601
Rebeca DJ Lopez <lozrey@gmail.com>
2019-11-30 16:52:33 -0600

    test EventEmitter from @angular/core

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,21 @@
 import { VoteComponent } from './vote.component'; 
 
-describe('VoteComponent', () => {
+
+describe('VOTE COMPONENT SUITE', () => {
   var component: VoteComponent; 
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should check if voteChante emmiter raises an event', () => {
+    // emmiters are observables
+    var votes = null;
+
+    component.voteChanged.subscribe( totalVotes => votes = totalVotes);
+
+    component.upVote();
+
+    expect(votes).toBe(1);
   });
 });
