85f23c5f871e9e55483814d8f92bbcac400ee44f
Rebeca DJ Lopez <lozrey@gmail.com>
2019-12-01 08:39:26 -0600

    test service, call add, add value, add error

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -2,9 +2,11 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service'; 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 
-describe('TodosComponent', () => {
+describe('TODOS COMPONENT SUITE', () => {
   let service: TodoService;
   let component: TodosComponent;
 
@@ -17,13 +19,46 @@ describe('TodosComponent', () => {
   it('should check if getTodos method is called onInit', () => {
 
     let todos = [1,2,3];
-    spyOn(service, 'getTodos').and.callFake( ()=> {
+    spyOn(service, 'getTodos').and.callFake( t => {
        return Observable.from([ todos ]);
     });
 
     component.ngOnInit();
 
     expect(component.todos).toBe(todos);
+  });
+
+  it('should check if add method is called', () => {
+
+    let spy = spyOn(service, 'add').and.callFake( t => {
+      // add method was called
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should check if component add method works', () => {
+    let todo = { id: 4 };
+    //spyOn overrides the service not the component
+    //and then the component uses this new service
+    spyOn(service, 'add').and.returnValue(Observable.from([ todo ]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should mock an error returning from the server', () => {
+    //here we mock an error from the server
+    let error = "error from the server";
+    spyOn(service, 'add').and.returnValue(Observable.throw(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
   });
 
 });
--- src/app/06-services/todos.component.ts
+++ src/app/06-services/todos.component.ts
@@ -3,6 +3,7 @@ import { TodoService } from './todo.service'
 
 export class TodosComponent { 
   todos: any[] = [];
+  // message property returns errors from the server
   message; 
 
   constructor(private service: TodoService) {}
@@ -15,6 +16,7 @@ export class TodosComponent {
     var newTodo = { title: '... ' };
     this.service.add(newTodo).subscribe(
       t => this.todos.push(t),
+      //this is the message property 
       err => this.message = err);
   }
 
