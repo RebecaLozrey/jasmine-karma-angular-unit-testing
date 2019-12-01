bc3e27bcbd4820e286b24fb4e829be29260b0ddc
Rebeca DJ Lopez <lozrey@gmail.com>
2019-11-30 18:08:24 -0600

    spyOn getTodos method in TodoService

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,29 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+
 
 describe('TodosComponent', () => {
+  let service: TodoService;
   let component: TodosComponent;
 
   beforeEach(() => {
+    // null or fake http service
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should check if getTodos method is called onInit', () => {
+
+    let todos = [1,2,3];
+    spyOn(service, 'getTodos').and.callFake( ()=> {
+       return Observable.from([ todos ]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos).toBe(todos);
   });
+
 });
